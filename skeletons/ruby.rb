aa48598f9d371ab0d688aa3530e8af12a9169d73
Addisu Z. Taddese <addisu@gmail.com>
2010-10-13 23:13:43 -0500

    Organized everything using pathogen

--- skeletons/ruby.rb
+++
@@ -1,8 +0,0 @@
-# <+FILE NAME+><+NOTE+>
-# @Author:  <+AUTHOR+> (<+EMAIL+>)
-# @Created: <+DATE+> <+TIME+>
-
-
-if __FILE__ == $0
-end
-
--- skeletons/shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# <+FILE NAME+> -- created <+DATE+>, <+AUTHOR+>
-# @Revision:    0.0
-
-<+CURSOR+>
-
-# vi: 
